73bf0a2b43fe1fa2c7a97db9e044c2b5f18b8f43
dmumford <github@davidmumford.co.uk>
2021-01-30 00:26:57 +0000

    initial commit

---
+++ flashcard.py
@@ -0,0 +1,171 @@
+# TODO
+# 1. Add audio
+
+import json
+# TKinter imports
+import tkinter as tk
+from tkinter import ttk
+from tkinter import *
+
+# global labels
+global n, cn, py, wt, div, en, c_num, nav_prev, nav_next
+
+n = 1
+
+# Opening JSON file
+f = open('db.json')
+
+# returns JSON object as
+# a dictionary
+data = json.load(f)
+
+
+# get card info from json
+def getCard(nav='n'):
+
+    global n, cn, py, wt, div, en, c_num, nav_prev, nav_next
+
+    # next card
+    if (nav == 'n'):
+        n = n + 1
+    # previous card
+    elif (nav == 'p'):
+        n = n - 1
+    # user specified card
+    else:
+        n = int(nav)
+
+    # take into account index 0
+    cNum = n - 1
+
+    # get new values
+    zh = (data[1]["data"][cNum]["zh"]).strip()
+    pinyin = (data[1]["data"][cNum]["pinyin"]).strip()
+    word_type = (data[1]["data"][cNum]["type"]).strip()
+    english = (data[1]["data"][cNum]["en"]).strip()
+    card_number = (data[1]["data"][cNum]["id"]).strip()
+
+    # update window with new values
+    cn.config(text=zh)
+    py.config(text=pinyin)
+    wt.config(text=word_type)
+    en.config(text=english)
+    c_num.config(text=card_number)
+
+
+zh = (data[1]["data"][0]["zh"]).strip()
+pinyin = (data[1]["data"][0]["pinyin"]).strip()
+word_type = (data[1]["data"][0]["type"]).strip()
+english = (data[1]["data"][0]["en"]).strip()
+card_number = (data[1]["data"][0]["id"]).strip()
+
+prevArrow = "  «  "
+nextArrow = "  »  "
+
+root = Tk()
+
+# This is the section of code which creates the main window
+root.geometry('700x600')
+root.configure(background='#f7f3d2')
+root.title('TLT Flashcards')
+
+# Chinese Character
+cn = tk.Label(
+    root,
+    text=zh,
+    # fg="light green",
+    bg="#f7f3d2",
+    pady=10,
+    font="Helvetica 70 bold")
+cn.pack()
+
+# Pinyin
+py = tk.Label(
+    root,
+    text=pinyin,
+    fg="#44168a",
+    bg="#f7f3d2",
+    # pady=10,
+    font="Helvetica 50")
+py.pack()
+
+# Word Type
+wt = tk.Label(root,
+              text=word_type,
+              fg="grey",
+              bg="#f7f3d2",
+              pady=20,
+              font="Helvetica 30 italic")
+wt.pack()
+
+# Divide line
+div = tk.Label(root,
+               text="________________________________________",
+               fg="darkgrey",
+               bg="#f7f3d2",
+               font="Helvetica 30")
+div.pack()
+
+# show English word
+en = Label(root,
+           text=english,
+           fg="#314282",
+           bg="#f7f3d2",
+           pady=25,
+           font="Helvetica 30")
+en.pack(expand=YES, fill=BOTH)
+
+# card number
+c_num = tk.Label(root,
+                 text=card_number,
+                 fg="white",
+                 bg="#7314C0",
+                 font="Helvetica 20 bold")
+c_num.pack()
+# place bottom-center
+c_num.place(relx=0.5, rely=0.95, anchor=CENTER)
+
+# Navigation arrows
+# previous arrow
+nav_prev = tk.Label(root,
+                    text=prevArrow,
+                    fg="white",
+                    bg="#7314C0",
+                    font="Helvetica 40 bold")
+# position left
+nav_prev.pack(side=LEFT)
+
+# next arrow
+nav_next = tk.Label(root,
+                    text=nextArrow,
+                    fg="white",
+                    bg="#7314C0",
+                    font="Helvetica 40 bold")
+# position left
+nav_next.pack(side=tk.RIGHT)
+
+# Card search box
+sv = StringVar()
+sv.trace("w", lambda name, index, mode, sv=sv: callback(sv))
+searchLabel = Label(text="card", fg="white", bg="#7314C0")
+searchLabel.place(relx=0.88, rely=0.01)
+search = Entry(textvariable=sv, bd=5, width=3)
+search.place(relx=1.0, rely=0.0, anchor=NE)
+
+
+# on search box update
+def callback(sv):
+    # remove non-numeric chars from input
+    query = ''.join(c for c in sv.get() if c.isdigit())
+    # print (query)
+    # go to card
+    getCard(query)
+
+# nav on click
+nav_next.bind('<Button-1>', lambda x: getCard())
+nav_prev.bind('<Button-1>', lambda x: getCard('p'))
+# nav on arrow keys
+root.bind('<Right>', lambda x: getCard())
+root.bind('<Left>', lambda x: getCard('p'))
+
+root.mainloop()
